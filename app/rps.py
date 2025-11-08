0666531c73d491975abca205325dd0c083af10fc
francis <ht462@georgetown.edu>
2025-11-08 18:18:26 -0500

    refactoring changes

---
+++ app/rps.py
@@ -0,0 +1,54 @@
+import random
+
+
+VALID_OPTIONS = ["rock", "paper", "scissors"]
+
+ # DETERMINE THE WINNER
+
+def determine_winner(u, c):
+    if u == "rock" and c == "rock":
+        return "TIE GAME"
+    elif u == "rock" and c == "paper":
+        return "COMPUTER WINS"
+    elif u == "rock" and c == "scissors":
+        return "USER WINS"
+    elif u == "paper" and c == "rock":
+        return "COMPUTER WINS" # OOPS
+    elif u == "paper" and c == "paper":
+        return "TIE GAME"
+    elif u == "paper" and c == "scissors":
+        return "USER WINS" # OOPS
+    elif u == "scissors" and c == "rock":
+        return "COMPUTER WINS"
+    elif u == "scissors" and c == "paper":
+        return "USER WINS"
+    elif u == "scissors" and c == "scissors":
+        return "TIE GAME"
+
+if __name__ == "__main__":
+    #only run code the code below if we are running this script from the command line
+
+    # ASK USER FOR AN INPUT (R/P/S)
+
+    user_choice = input("Please choose one of 'rock', 'paper', or 'scissors': ")
+    print("USER:", user_choice)
+
+    # VALIDATIONS
+
+    if user_choice not in VALID_OPTIONS:
+        print("OOPS INVALID INPUT, PLEASE TRY AGAIN")
+        # exit()
+        quit()
+
+    # GENERATE RANDOM COMPUTER CHOICE
+
+    computer_choice = random.choice(VALID_OPTIONS)
+    print("COMP:", computer_choice)
+
+
+    result = determine_winner(user_choice, computer_choice)
+    print(result)
+
+# assert determine_winner("rock", "rock") == "TIE GAME"
+
+
